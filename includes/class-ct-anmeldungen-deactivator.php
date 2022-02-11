c29afb9ed541b4801dc24e97a4b7f007da998a35
Lukas Dumberger <lukas.dumberger@exxcellent.de>
2022-02-11 11:13:45 +0100

    feat(deactivate-plugin): remove options when plugin is deactivated

--- includes/class-ct-anmeldungen-deactivator.php
+++ includes/class-ct-anmeldungen-deactivator.php
@@ -30,7 +30,19 @@ class Ct_Anmeldungen_Deactivator {
 	 * @since    1.0.0
 	 */
 	public static function deactivate() {
-
+        self::deleteOptions([
+            Ct_Anmeldungen_Admin::$OPTION_URL,
+            Ct_Anmeldungen_Admin::$OPTION_GROUP_HASH,
+            Ct_Anmeldungen_Admin::$OPTION_CHILD_TEMPLATE,
+            Ct_Anmeldungen_Admin::$OPTION_PARENT_TEMPLATE
+        ]);
 	}
+
+	private static function deleteOptions(array $options){
+        foreach($options as $option){
+            $optionHasBeenDeleted = delete_option($option);
+            Ct_Anmeldungen::$LOG->info("Option ". $option . " has" . ($optionHasBeenDeleted ? ' ' : ' not ') . "been deleted.");
+        }
+    }
 
 }
